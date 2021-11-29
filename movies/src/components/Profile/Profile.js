88b08c2955e2c9c6b648a6b7eae80b0a1ede69f9
Vladimir Marsov <marsov012@gmail.com>
2021-11-29 23:46:03 +0200

    bugfix/button edit in the Profile allowed to send the same data

--- movies/src/components/Profile/Profile.js
+++ movies/src/components/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {currentUserContext} from '../../utils/Contexts';
 import './Profile.css';
 import Header from '../Header/Header';
@@ -7,13 +7,31 @@ import { useFormWithValidation } from '../../utils/FormValidator';
 
 function Profile({ onUpdateUser, onExit, isLoading }) {
   const currentUser = React.useContext(currentUserContext);
+  const [isDataEdited, setIsDataEdited] = useState(false);
+  const nameRef = React.useRef();
+  const emailRef = React.useRef();
   const FormWithValidation = useFormWithValidation();
   const { values, handleChange, errors, isValid } = FormWithValidation;
+
+  React.useEffect(() => {
+    currentUser.name === nameRef.current.value && currentUser.email === emailRef.current.value ? setIsDataEdited(false) : setIsDataEdited(true);
+  }, [currentUser.email, currentUser.name])
+
+  const handleEdit = (e) => {
+    handleChange(e);
+    currentUser.name === nameRef.current.value && currentUser.email === emailRef.current.value ? setIsDataEdited(false) : setIsDataEdited(true);
+  }
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
     if(isValid) {
-      onUpdateUser({ name: values.name, email: values.email });
+      // Если пользователь не менял значение поля, то values[имя поля] будет undefined
+      // В этом случае устанавливаем ему текущее значение инпута.
+      const userData = { 
+        name: !values.name ? nameRef.current.value : values.name,
+        email: !values.email ? emailRef.current.value : values.email,
+      };
+      onUpdateUser(userData);
     }
   };
 
@@ -27,7 +45,7 @@ function Profile({ onUpdateUser, onExit, isLoading }) {
             <div className="form__input-box">
               <div className="form-input-wrapper">
                 <label htmlFor="name-input" className="form__input-name">Имя</label>
-                <input id="name-input" onChange={handleChange} type="text" placeholder="Имя" className="form__input" name="name" required minLength="2"  maxLength="40" defaultValue={currentUser.name}/>
+                <input id="name-input" ref={nameRef} onChange={handleEdit} type="text" placeholder="Имя" className="form__input" name="name" required minLength="2"  maxLength="40" defaultValue={currentUser.name}/>
               </div>
               <span className="form__error name-input-error">{errors.name}</span>
             </div>
@@ -35,12 +53,12 @@ function Profile({ onUpdateUser, onExit, isLoading }) {
              <div className="form__input-box">
                <div className="form-input-wrapper">
                  <label htmlFor="email-input" className="form__input-name">Email</label>
-                 <input id="email-input"  type="email" onChange={handleChange} placeholder="Email" className="form__input" name="email" required minLength="2" maxLength="30" defaultValue={currentUser.email}/>
+                 <input id="email-input" ref={emailRef}  type="email" onChange={handleEdit} placeholder="Email" className="form__input" name="email" required minLength="2" maxLength="30" defaultValue={currentUser.email}/>
                </div>
               <span className="form__error">{errors.email}</span>
              </div>
 
-            <button type="submit" className="form__button form__button_component_profile" disabled={!isValid}>Редактировать
+            <button type="submit" className="form__button form__button_component_profile" disabled={!isValid || !isDataEdited}>Редактировать
             </button>
             <button type="button" onClick={(e) => {onExit(e)}} className="form__link-button form__link-button_type_logout">Выйти из аккаунта
             </button>
--- movies/src/utils/FormValidator.js
+++ movies/src/utils/FormValidator.js
@@ -54,5 +54,5 @@ export function useFormWithValidation() {
     [setValues, setErrors, setIsValid]
   );
 
-  return { values, handleChange, errors, isValid, resetForm };
+  return { values, handleChange, errors, isValid, resetForm, setValues };
 }
